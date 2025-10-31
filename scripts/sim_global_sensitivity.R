d15813dc8ae125451cf6dcea92936ee814604f0e
“Andreas <eich.andreas@web.de>
2025-10-31 13:15:26 -0400

    Added docu, added max metric, updated names of metrics

--- scripts/sim_global_sensitivity.R
+++ scripts/sim_global_sensitivity.R
@@ -1,9 +1,13 @@
 run_sims <- function(param_sets) {
+  # empty df to store results
   results <- data.frame()
 
+  #loop over each row in parameter df
   for (i in 1:nrow(param_sets)) {
+    # select one row of parameters
     pars <- as.list(param_sets[i, ])
 
+    # simulate with this set of parameters
     out <- ode(
       y = state,
       times = times,
@@ -12,18 +16,27 @@ run_sims <- function(param_sets) {
       forcing_tbl = dat_forc
     )
 
+    # transforl deSolve object to df
     df <- as.data.frame(out)
 
+    # Calculate metrics:
+    # - mean Phyto_bm
+    # - minimum Phyto_bm
+    # - maximum Phyto_bm
+    # - time (day) of maximum
+    # add actual values of parameter set
     metrics <- data.frame(
       p = pars$p,
       I_min = pars$I_min,
       r = pars$r,
       R_0 = pars$R_0,
       g = pars$g,
-      Average = mean(df$Phyto_bm),
-      Minimum = min(df$Phyto_bm),
-      Timing = times[which.max(df$Phyto_bm)]
+      mean = mean(df$Phyto_bm),
+      min = min(df$Phyto_bm),
+      max = max(df$Phyto_bm),
+      time_max = times[which.max(df$Phyto_bm)]
     )
+    # combine into "growing" df
     results <- rbind(results, metrics)
   }
   return(results)
